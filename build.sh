bd1b173a2af1b811a6d4e87ba2b4466011af2a63
Sven Van Caekenberghe <svc@mac.com>
2015-08-27 17:21:49 +0200

    don't use relative paths

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#../bin/pharo build.image config http://mc.stfx.eu/MyRepository ConfigurationOfMyApplicationServer --username=deploy@acme.com --password=secret --install=bleedingEdge
-../bin/pharo build.image config http://mc.stfx.eu/Neo ConfigurationOfNeoConsole --install=stable
+#~/pharo/bin/pharo build.image config http://mc.stfx.eu/MyRepository ConfigurationOfMyApplicationServer --username=deploy@acme.com --password=secret --install=bleedingEdge
+~/pharo/bin/pharo build.image config http://mc.stfx.eu/Neo ConfigurationOfNeoConsole --install=stable
--- build.sh.m4
+++ build.sh.m4
@@ -1,3 +1,3 @@
 #!/bin/bash
-../bin/pharo build.image config _CONFIG_REPO_ _CONFIG_NAME_ --username=_CONFIG_USER_ --password=_CONFIG_PASS_ --install=_CONFIG_VERSION_
-../bin/pharo build.image save _IMAGE_NAME_
+~/pharo/bin/pharo build.image config _CONFIG_REPO_ _CONFIG_NAME_ --username=_CONFIG_USER_ --password=_CONFIG_PASS_ --install=_CONFIG_VERSION_
+~/pharo/bin/pharo build.image save _IMAGE_NAME_
