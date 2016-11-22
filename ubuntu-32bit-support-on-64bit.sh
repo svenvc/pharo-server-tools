bff0df999626e98516f29b6a8e1c1a6f63b8103e
Sven Van Caekenberghe <sven@beta9.be>
2016-11-22 15:55:30 +0100

    Update ubuntu-32bit-support-on-64bit.sh
    Added info about UI deps

--- ubuntu-32bit-support-on-64bit.sh
+++ ubuntu-32bit-support-on-64bit.sh
@@ -4,3 +4,5 @@ sudo apt-get update
 sudo apt-get install libc6:i386
 sudo apt-get install libssl1.0.0:i386
 sudo apt-get install libfreetype6:i386
+# for UI support, add the following
+# sudo apt-get install libgl1-mesa-glx:i386
