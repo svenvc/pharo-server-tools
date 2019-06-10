128c3b972d98f8ed900c458c2b8df812185ecc49
Jonathan van Alteren <jvalteren@objectguild.com>
2019-06-10 17:04:08 +0200

    Changed install script to use 64-bit Pharo 7.0 image. Added statement to copy sources file.

--- install-pharo.sh
+++ install-pharo.sh
@@ -2,13 +2,12 @@
 script_home=$(dirname $0)
 script_home=$(cd $script_home && pwd)
 sudo apt-get install unzip 
-curl get.pharo.org/40+vm | bash
+curl get.pharo.org/64/70+vm | bash
 rm $script_home/pharo-ui
-mkdir ~/pharo
-mkdir ~/pharo/bin
-mkdir ~/pharo/build
-mv $script_home/Pharo.image ~/pharo/build
+mkdir -p ~/pharo/bin ~/pharo/build
+mv $script_home/Pharo7.0-32bit-*.sources ~/pharo/build
 mv $script_home/Pharo.changes ~/pharo/build
+mv $script_home/Pharo.image ~/pharo/build
 cp $script_home/build.sh ~/pharo/build
 mv $script_home/pharo ~/pharo/bin
 mv $script_home/pharo-vm ~/pharo/bin
